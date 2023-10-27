3a6bbff706de68f2bd2f82692c7465a24f289df0
Cyphle <cyril.phamle@gmail.com>
2023-10-27 20:51:01 +0200

    query params

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,64 @@ import { createSchema, createYoga } from 'graphql-yoga'
 import { expressConfig } from './config/express.config';
 import { Express } from 'express';
 
-export const schema = createSchema({
-  typeDefs: `
-    type Query {
-      title: String!
-      price: Float!
-      releaseYear: Int
-      rating: Float
-      inStock: Boolean!
-    }
-  `,
-  resolvers: {
-    Query: {
-      title: () => 'The War of Art',
-      price: () => 12.99,
-      releaseYear: () => 2007,
-      rating: () => 5,
-      inStock: () => true
+const typeDefs = `
+  type Query {
+    greeting(name: String, position: String): String!
+    add(numbers: [Float!]!): Float!
+    grades: [Int!]!
+    me: User!
+    post: Post!
+  }
+  
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    age: Int
+  }
+  
+  type Post {
+    id: ID!
+    title: String!
+    body: String!
+    published: Boolean!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    greeting(parent: any, args: any, ctx: any, info: any) {
+      return `Hello, ${args.name} World! You are in ${args.position}`;
+    },
+    add(parent: any, args: any, ctx: any, info: any) {
+      return args.numbers.reduce((accumulator: number, currentValue: number) => {
+        return accumulator + currentValue;
+      }, 0.0);
+    },
+    grades(parent: any, args: any, ctx: any, info: any) {
+      return [99, 80, 93];
+    },
+    me() {
+      return {
+        id: '123098',
+        name: 'Mike',
+        email: 'mike@example.com',
+      }
+    },
+    post() {
+      return {
+        id: '092',
+        title: 'GraphQL 101',
+        body: '',
+        published: false
+      }
     }
   }
+};
+
+export const schema = createSchema({
+  typeDefs: typeDefs,
+  resolvers: resolvers
 });
 
 // GraphQL Yoga
